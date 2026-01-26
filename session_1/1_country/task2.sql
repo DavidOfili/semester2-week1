6f59a2f3808909cf7a7f6d0d4c98f3fcefbcd0f7
DavidOfili <125293600+DavidOfili@users.noreply.github.com>
2026-01-26 08:19:42 +0000

    task2.sql completed up to No. 6

--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -11,4 +11,12 @@
 
 
 -- write your sql code here
-
+1. SELECT Country FROM countries ORDER BY Population DESC LIMIT 10;
+2. SELECT Country FROM countries ORDER BY AreaSqMi ASC LIMIT 5;
+3. SELECT Country FROM countries WHERE Continent = 'Europe' ORDER BY AreaSqMi DESC LIMIT 1;
+Country;
+4. SELECT Country FROM countries WHERE Continent = 'Africa' ORDER BY AreaSqMi ASC LIMIT 3;
+Country;
+5. SELECT Country FROM countries ORDER BY LiteracyPercent LIMIT 5;
+6. SELECT Country FROM countries WHERE Country LIKE 'C%' ORDER BY GDPPerCapita LIMIT 3;
+Country;
